4d9013bda0af966580ab09fdb0335979b7e4612e
Thidas Senavirathna <thidass@seylan.lk>
2026-02-03 09:51:52 +0530

    design : change the bg vid

--- frontend/app/components/hero/Hero.jsx
+++ frontend/app/components/hero/Hero.jsx
@@ -30,7 +30,7 @@ const Hero = () => {
                 loop
                 playsInline
             >
-                <source src="/hero-video.mp4" type="video/mp4" />
+                <source src="/hero-video-money.mp4" type="video/mp4" />
                 Your browser does not support the video tag.
             </video>
 
